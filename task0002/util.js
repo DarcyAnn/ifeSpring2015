e4dedd3781a9780e4266df57d5091955fc9d43a4
WarmRabbit <543138020@qq.com>
2016-05-28 22:37:57 +0800

    add other methods for getClass()

--- task0002/util.js
+++ task0002/util.js
@@ -3,7 +3,7 @@
  */
 
 /*
-    2. JavaScript数据类型及语言基础
+ 2. JavaScript数据类型及语言基础
  */
 // 判断arr是否为一个数组，返回一个bool值
 function isArray(arr) {
@@ -172,7 +172,7 @@ each(arr, output2);  // 0:java, 1:c, 2:php, 3:html
 // 获取一个对象里面第一层元素的数量，返回一个整数
 function getObjectLength(obj) {
     var count = 0;
-    for(x in obj){
+    for (x in obj) {
         count++;
     }
     return count;
@@ -193,7 +193,7 @@ console.log(getObjectLength(obj)); // 3
 // 判断是否为邮箱地址
 function isEmail(emailStr) {
     // your implement
-    var re =/^\w+(\.\w+)*@\w+([\.]\w+)+$/;
+    var re = /^\w+(\.\w+)*@\w+([\.]\w+)+$/;
     return re.test(emailStr);
 }
 console.log(isEmail('abx123@qq123.com'));
@@ -211,69 +211,133 @@ console.log(isMobilePhone('10000000000'));
 console.log(isMobilePhone('188000'));
 
 
-
 /*
-    3. DOM
+ 3. DOM
  */
+//对元素中className的增删改查
+var myEle = document.getElementById("number1");
+var myEle2 = document.getElementById("number2");
+var addSpan = document.getElementById("addSpan")
+
+//查找element中是否有名为exitClassName的样式
+//方法一、用正则表达式
+function hasClass1(element, exitClassName) {
+    var regexp = new RegExp('(\\s|^)' + exitClassName + '(\\s|$)');
+    return regexp.test(element.className);
+}
+
+//方法二、用正则表达式
+function hasClass2(element, exitClassName) {
+    var regexp = new RegExp('( |)' + exitClassName);
+    return regexp.test(element.className)
+}
+
+//方法三、用indexOf()
+function hasClass3(element, exitClassName) {
+    if (element.className.indexOf(exitClassName) === -1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(myEle.className);
+console.log('exit?' + hasClass2(myEle, 'abc'))//true;
+console.log('exit?' + hasClass2(myEle, 'mn'))//false;
+
 // 为element增加一个样式名为newClassName的新样式
 function addClass(element, newClassName) {
     // your implement
-    if (element.className){
+    if (element.className) {
         element.className = element.className + " " + newClassName;
     } else {
         element.className = newClassName;
     }
 }
-var myEle = document.getElementById("number1");
-var myEle2 = document.getElementById("number2");
-var addSpan = document.getElementById("addSpan")
-addClass(myEle,"def");
-addClass(myEle,"abc2");
+
+addClass(myEle, "def");
+addClass(myEle, "abc2");
 console.log(myEle.className);
 
 // 移除element中的样式oldClassName
-function removeClass(element, oldClassName) {
+function removeClass1(element, oldClassName) {
     // your implement
-    var reg = new RegExp('\\s|^'+oldClassName+'\\s|$');
-    element.className = element.className.replace(reg,' ');
+    var reg = new RegExp('\\s|^' + oldClassName + '\\s|$');
+    element.className = element.className.replace(reg, ' ');
 }
 
-function removeClass2(element,oldClassName){
-    var reg = new RegExp('\\s|^'+oldClassName+'\\s|$');
+function removeClass2(element, oldClassName) {
+    var reg = new RegExp('\\s|^' + oldClassName + '\\s|$');
     var arr = element.className.split(reg);
     element.className = arr.join(' ');
 }
 
-removeClass2(myEle,"abc");
+removeClass2(myEle, "abc");
 console.log(myEle.className);
+
+// 替换element中的样式oldClassName为newClassName
+function replaceClass(element, oldClassName, newClassName) {
+    if (hasClass1(element, oldClassName)) {
+        removeClass1(element, oldClassName);
+        addClass(element, newClassName);
+    }
+    else addClass(element, newClassName);
+}
+
+replaceClass(myEle, 'abc', 'hahaha');
+console.log('replaceClass:' + myEle.className);
+
 
 // 判断siblingNode和element是否为同一个父元素下的同一级的元素，返回bool值
 function isSiblingNode(element, siblingNode) {
     // your implement
-    if (element.parentNode == siblingNode.parentNode){
+    if (element.parentNode == siblingNode.parentNode) {
         return true;
     } else {
         return false;
     }
 }
 
-console.log(isSiblingNode(myEle,myEle2));
-console.log(isSiblingNode(myEle,addSpan));
+console.log(isSiblingNode(myEle, myEle2));
+console.log(isSiblingNode(myEle, addSpan));
 
 
 // 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
 function getPosition(element) {
     // your implement
     var x = element.offsetLeft;
-
     var y = element.offsetTop;
-    if (element.offsetParent){
+    if (element.offsetParent) {
         x += getPosition(element.offsetParent).x;
         y += getPosition(element.offsetParent).y
     }
-    var pos = {"x":x,"y":y}
+    var pos = {"x": x, "y": y}
     return pos;
 }
 
 console.log(getPosition(myEle));
 console.log(getPosition(addSpan));
+
+//接下来挑战一个mini $，它和之前的$是不兼容的，它应该是document.querySelector的功能子集，
+// 在不直接使用document.querySelector的情况下，在你的util.js中完成以下任务：
+
+// 实现一个简单的Query
+//
+
+
+// 可以通过id获取DOM对象，通过#标示，例如
+console.log($("#adom")); // 返回id为adom的DOM对象
+
+// 可以通过tagName获取DOM对象，例如
+$("a"); // 返回第一个<a>对象
+
+// 可以通过样式名称获取DOM对象，例如
+$(".classa"); // 返回第一个样式定义包含classa的对象
+
+// 可以通过attribute匹配获取DOM对象，例如
+$("[data-log]"); // 返回第一个包含属性data-log的对象
+
+$("[data-time=2015]"); // 返回第一个包含属性data-time且值为2015的对象
+
+// 可以通过简单的组合提高查询便利性，例如
+$("#adom .classa"); // 返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
