52bf8889472e74a3923a44d68a963933b30957f8
WarmRabbit <543138020@qq.com>
2016-06-05 09:55:18 +0800

    finished 1D carousel with setTimeout

--- task0002/js/task0002_3.js
+++ task0002/js/task0002_3.js
@@ -4,8 +4,8 @@
 (function () {
     var carousel = document.getElementById('carousel'),
         item = document.getElementById('item');
-    var imgWidth = window.getComputedStyle(carousel.children[0]).width;
-
+    var imgWidth = parseInt(window.getComputedStyle(carousel.children[0]).width);
+    var timer = 0;
     function addEvent(element, event, listener) {
         // your implement
         if (element.addEventListener) {
@@ -17,25 +17,53 @@
         }
     }
 
+    window.onresize = function () {
+        imgWidth = parseInt(window.getComputedStyle(carousel.children[0]).width);
+    };
+
     //鼠标移入切换
     function changeImage() {
-        imgWidth = parseInt(window.getComputedStyle(carousel.children[0]).width);
-        window.onresize = function () {
-            imgWidth = parseInt(window.getComputedStyle(carousel.children[0]).width);
-        };
 
-        for (var i = 0; i < item.children.length; i++) {
+        for (var p = 0; p < item.children.length; p++) {
             (function (i) {
                 item.children[i].onmouseover = function () {
+                    clearTimeout(timer);
                     carousel.style.left = -(imgWidth * i) + 'px';
-                    console.log(carousel.style.left);
-                    //carousel.children[i].style.zIndex = 3;
-                }
-            })(i);
+                    for(var n = 0 ; n < item.children.length ; n++){
+                        item.children[n].style.backgroundColor = '#fff';
+                    }
+                    item.children[i].style.backgroundColor = '#848484';
+                };
+
+                item.children[i].onmouseout = function () {
+                    clearTimeout(timer);
+                    b = i+1;
+                    timer = setTimeout(function () {
+                        autoChange(b);
+                    },2500);
+                };
+            })(p);
         }
     }
 
     window.onload = changeImage();
+
     //自动轮播
+    function autoChange(count){
+        clearTimeout(timer);
+        carousel.style.left = -(imgWidth * (count % 3)) + 'px';
+        for(var m = 0 ; m < item.children.length ; m++){
+            item.children[m].style.backgroundColor = '#fff';
+        }
+        item.children[count%3].style.backgroundColor = '#848484';
+        timer = setTimeout(function () {
+            var q = count + 1;
+            autoChange(q);
+        },2500);
+    }
+
+    timer = setTimeout(function () {
+        autoChange(0);
+    },2500);
 
 })();
