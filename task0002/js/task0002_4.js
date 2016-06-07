be9b87e5e042a9bff500e142d198bb5365d9ff79
WarmRabbit <543138020@qq.com>
2016-06-07 09:09:41 +0800

    add onclick

--- task0002/js/task0002_4.js
+++ task0002/js/task0002_4.js
@@ -1,22 +1,37 @@
 /**
  * Created by Administrator on 2016-6-6.
  */
-(function (){
+(function () {
     var suggestData = ['Simon', 'Siri', 'Smith'];
     var length = suggestData.length;
     var hintLi = [];
     var content = document.getElementById('content');
     var hintUl = document.getElementById('hint');
-    content.onfocus = function(){
-        for(var i = 0 ;i < length ; i++){
+    var bool = false;
+    content.onfocus = function () {
+        for (var i = 0; i < length; i++) {
             hintLi[i] = document.createElement('li');
             hintLi[i].innerHTML = suggestData[i];
             hintUl.appendChild(hintLi[i]);
         }
         hintUl.style.display = 'block';
+        bool = true;
     }
-    content.onblur = function(){
+    content.onblur = function () {
         hintUl.innerHTML = null;
         hintUl.style.display = 'none';
     }
+
+
+    if (bool) {
+        for (var j = 0; j < length; j++) {
+            (function (p) {
+                hintUl.onclick = function () {
+                    content.value = hintLi[p].innerHTML;
+                }
+            })(j);
+        }
+    }
+
+
 })();
