fae59e921b1a42a32e650758fb19c268abe6a76c
WarmRabbit <543138020@qq.com>
2016-06-09 23:47:13 +0800

    no promise

--- task0002/js/task0002_4.js
+++ task0002/js/task0002_4.js
@@ -7,27 +7,26 @@
     var hintLi = [];
     var content = document.getElementById('content');
     var hintUl = document.getElementById('hint');
-    content.onfocus = function (event,callback) {
+    content.onfocus = function () {
         for (var i = 0; i < length; i++) {
             hintLi[i] = document.createElement('li');
             hintLi[i].innerHTML = suggestData[i];
             hintUl.appendChild(hintLi[i]);
         }
         hintUl.style.display = 'block';
-        if (callback) {
-            fillForm();
-        }
         function fillForm() {
             for (var j = 0; j < length; j++) {
                 (function (p) {
+                    //这里用onclick不起作用，原因是先触发了content元素的onblur事件，ul隐藏了。
                     hintLi[p].onmousedown = function () {
                         content.value = hintLi[p].innerHTML;
                     }
                 })(j);
             }
         }
+        fillForm();
     }
-    //这里用onclick不起作用，原因是先触发了content元素的onblur事件，ul隐藏了。
+
     content.onblur = function () {
         hintUl.innerHTML = null;
         hintUl.style.display = 'none';
