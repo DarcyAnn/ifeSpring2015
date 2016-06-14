3d58427dc6e58f7571d189167578dcf19ef7702c
WarmRabbit <543138020@qq.com>
2016-06-14 23:58:38 +0800

    fix TT(cry)

--- task0002/js/task0002_5.js
+++ task0002/js/task0002_5.js
@@ -14,7 +14,9 @@
         originClientY = 0,
         originCount = length,
         targetCount = 0,
-        itemTop = 0;//左边某个小方块的top值
+        itemTop = 0,//左边某个小方块的top值
+        itemHeight = leftOuter.children[0].offsetHeight,
+        rightItemTop = 0;//右边某个小方块的top值
 
     window.onload = drag;
     function drag() {
@@ -25,17 +27,13 @@
                         originClientX = event.clientX;
                         originClientY = event.clientY;
                         itemTop = leftOuter.children[m].offsetTop;
-                        console.log('clientX:' + event.clientX + ';offsetLeft:' + leftOuter.children[m].offsetLeft + ';pointLeft:' + pointLeft);
-                        console.log('clientY:' + event.clientY + ';offsetTop:' + leftOuter.children[m].offsetTop + ';pointTop:' + pointTop);
 
                         leftOuter.children[m].onmousemove = function (event) {
                             leftOuter.children[m].style.left = event.clientX - originClientX + 'px';
                             leftOuter.children[m].style.top = event.clientY - originClientY + 'px';
-                            console.log('clientX:' + event.clientX + ';targetLeft:' + targetLeft);
                             if ((leftOuter.children[m].offsetLeft + 80) > targetLeft) {
                                 leftOuter.children[m].style.left = targetLeft - originLeft + 'px';
-                                console.log('height:' + leftOuter.children[m].offsetHeight + '!:' + targetCount * leftOuter.children[m].offsetHeight);
-                                leftOuter.children[m].style.top = targetTop + targetCount * leftOuter.children[m].offsetHeight - itemTop + 'px';
+                                leftOuter.children[m].style.top = targetTop + targetCount * itemHeight - itemTop + 'px';
                                 originCount--;
                                 targetCount++;
                                 leftOuter.children[m].onmousemove = null;
@@ -48,6 +46,8 @@
                     } else {//从右边移到左边
                         originClientX = event.clientX;
                         originClientY = event.clientY;
+                        itemTop = leftOuter.children[m].offsetTop;
+                        rightItemTop = parseInt(leftOuter.children[m].style.top);
                         console.log('clientX:' + event.clientX + ';offsetLeft:' + leftOuter.children[m].offsetLeft + ';pointLeft:' + pointLeft);
                         console.log('clientY:' + event.clientY + ';offsetTop:' + leftOuter.children[m].offsetTop + ';pointTop:' + pointTop);
                         console.log('left:' + leftOuter.children[m].style.left + ';originLeft:' + originLeft);
@@ -55,11 +55,12 @@
                         leftOuter.children[m].onmousemove = function (event) {
                             leftOuter.children[m].style.left = targetLeft - originLeft - (originClientX - event.clientX) + 'px';
                             //下面这行有问题，再看看哇
-                            leftOuter.children[m].style.top = targetTop + (targetCount-1) * leftOuter.children[m].offsetHeight - itemTop + event.clientY - originClientY + 'px';
+
+                            leftOuter.children[m].style.top =  rightItemTop + event.clientY - originClientY + 'px';
                             console.log('left:' + leftOuter.children[m].style.left + ';originLeft:' + originLeft);
-                            if (leftOuter.children[m].offsetLeft  < (originLeft + 80)) {
+                            if (leftOuter.children[m].offsetLeft < (originLeft + 80)) {
                                 leftOuter.children[m].style.left = 0;
-                                console.log('height:' + leftOuter.children[m].offsetHeight + '!:' + targetCount * leftOuter.children[m].offsetHeight);
+                                console.log('height:' + itemHeight + '!:' + targetCount * itemHeight);
                                 leftOuter.children[m].style.top = 0;
                                 originCount++;
                                 targetCount--;
